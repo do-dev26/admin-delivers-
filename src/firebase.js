da494039f269921a8999675ec076c2fe2d5c1698
do-dev26 <scrollbreaker83@gmail.com>
2026-09-25 12:34:32 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBsAEpp3hooCCHYEdmTNqTId9aWK0a69Ms",
-  authDomain: "ai-vit-52666.firebaseapp.com",
-  projectId: "ai-vit-52666",
-  storageBucket: "ai-vit-52666.firebasestorage.app",
-  messagingSenderId: "137839510307",
-  appId: "1:137839510307:web:c8f56f623c8234b4638296",
-  measurementId: "G-CDTY9LJLC7"
-};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
